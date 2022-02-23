8c4b5d0526aa11383954dec0ff8d9ea87dec4f67
Catatomik <cata@catadev.ga>
2022-02-23 02:43:19 +0100

    ⚡✨ Use new graph adjacency matrix structure

--- src/FootPaths.ts
+++ src/FootPaths.ts
@@ -1,60 +1,58 @@
 import { node, WeightedGraph } from './utils/Graph';
 
+export type path = node[];
 /**
  * @description Generate the shortest paths from source node s to every nodes in graph G.
  * @param G Source graph
  * @param s Source node
  * @returns First, the distances from source node to considered node. Secondly, an array that traces downwards the shortest path from considered node to source node.
  */
-export default function Dijkstra(G: WeightedGraph, s: node): [number[], node[]];
+export function Dijkstra(G: WeightedGraph, s: node): [Map<node, number>, Map<node, node>];
 /**
  * @description Computes the shortest path from source node s to target node t.
  * @param G Source graph
  * @param s Source node
  * @param t Target node
+ * @returns The shortest path from s to t.
  */
-export default function Dijkstra(G: WeightedGraph, s: node, t: node): node[];
-export default function Dijkstra(G: WeightedGraph, s: node, t?: node): node[] | [number[], node[]] {
+export function Dijkstra(G: WeightedGraph, s: node, t: node): path;
+export function Dijkstra(G: WeightedGraph, s: node, t?: node): path | [Map<node, number>, Map<node, node>] {
 
-    const nodes: node[] = G.nodes;
-
-    const dist: Array<number> = new Array(nodes.length);
-    const prev: Array<node> = new Array(nodes.length);
+    const dist: Map<node, number> = new Map();
+    const prev: Map<node, node> = new Map();
     const Q: Set<node> = new Set();
 
-    for (let e of nodes) {
+    for (const e of G.nodes) {
 
-        dist[e] = Infinity;
-        prev[e] = null;
+        dist.set(e, Infinity);
+        prev.set(e, null);
         Q.add(e);
 
     }
-    dist[s] = 0;
+    dist.set(s, 0);
 
     while (Q.size) {
 
-        const u: node = ((): node => {
-            let min: [node, number] = [null, Infinity];
-            for (let e of Q) {
-                const d: number = dist[e];
-                if (d < min[1]) min = [e, d];
-            }
-            return min[0];
-        })()
+        let min: [node, number] = [null, Infinity];
+        for (const e of Q) {
+            const d: number = dist.get(e);
+            if (d <= min[1]) min = [e, d];
+        }
+        const u: node = min[0];
 
         Q.delete(u);
 
         if (t && u === t) break;
 
-        for (let v of G.neighbors(u)) {
+        for (const v of G.neighbors(u)) {
 
             if (!Q.has(v)) continue;
 
-            const alt = dist[u] + G.weight(u, v);
-            if (alt < dist[v]) {
+            const alt = dist.get(u) + G.weight(u, v);
+            if (alt < dist.get(v)) {
 
-                dist[v] = alt;
-                prev[v] = u;
+                dist.set(v, alt);
+                prev.set(v, u);
 
             }
 
@@ -64,21 +62,32 @@ export default function Dijkstra(G: WeightedGraph, s: node, t?: node): node[] | 
 
     if (t) {
 
-        let path: Array<node> = [];
-        let e: node = t;
-        if (prev[e] || e === s) {
-
-            while (e != null) {
-
-                path = [e, ...path];
-                e = prev[e];
-
-            }
-
-        }
-
-        return path;
+        return tracePath(prev, t, s);
 
     } else return [dist, prev];
+
+}
+
+/**
+ * @description Get the path from source to target.
+ * @param prev The second return value of Dijkstra, specific to the source.
+ * @param target 
+ * @param source 
+ */
+export function tracePath(prev: Map<node, node>, target: node, source?: node): path {
+
+    let path: path = [];
+    let e: node = target;
+    if (prev.get(e) || (source && e === source)) {
+
+        while (e != null) {
+
+            path = [e, ...path];
+            e = prev.get(e);
+
+        }
+    }
+
+    return path;
 
 }
