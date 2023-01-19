ef06afa71b39a570d356960f9165ae61b4a99318
Catatomik <cata@catadev.org>
2023-01-19 23:01:15 +0100

    💩

--- src/test/utils/ultils.ts
+++ src/test/utils/ultils.ts
@@ -16,10 +16,3 @@ export class Deferred<T = unknown> {
 
   }
 }
-
-/**
- * @description Checks unicity of a value in an array
- */
-export function unique<T>(v: T, i: number, arr: T[]): boolean {
-  return arr.indexOf(v) === i;
-}
