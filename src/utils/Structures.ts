0e2470a97dcf6405896cd93ef652b848182cfcdf
Catatomik <cata@catadev.org>
2023-02-09 10:14:30 +0100

    🎨🚧🐛 Migrate from arrays to maps

--- src/utils/Structures.ts
+++ src/utils/Structures.ts
@@ -1,65 +1,42 @@
 //A timestamp representation of a Date ; in milliseconds.
 export type timestamp = number;
+export type Id = number | string;
 
 /**
  * @description A Trip, i.e. a succession of stop times.
  */
-export class Trip {
-  readonly stopTimes: timestamp[];
-  readonly stopDurations: number[];
-
+export interface Trip {
+  id: Id;
   /**
-   * @param stopTimes Time of arrival at each stop.
-   * @param stopDurations Duration before departure, in milliseconds.
+   * @param stopTimes Time of arrival & departure at each stop.
    */
-  constructor(stopTimes: timestamp[], stopDurations: number[]) {
-    this.stopTimes = stopTimes;
-    this.stopDurations = stopDurations;
-  }
+  times: [timestamp, timestamp][];
 }
 
 export type footPaths = Map<stopId, Array<{ to: stopId; length: number }>>;
 
-export type stopId = number;
-
+export type stopId = Id;
 /**
- * @description A Stop, i.e. a geographical specific point that is connected to a route.
+ * @description A Stop, i.e. a geographical specific point that is connected to routes.
  */
-export class Stop {
+export interface Stop {
+  readonly id: stopId;
   readonly lat: number;
   readonly long: number;
   readonly connectedRoutes: Array<routeId>;
   readonly transfers: footPaths;
-  readonly id: any;
-
-  /**
-   * @description Creates a new Stop.
-   */
-  constructor(lat: number, long: number, connections: Array<routeId>, footPaths: footPaths, id: any) {
-    this.lat = lat;
-    this.long = long;
-    this.connectedRoutes = connections;
-    this.transfers = footPaths;
-    this.id = id;
-  }
 }
 
-export type routeId = number;
-
+export type routeId = Id;
 /**
  * @description A Route, i.e. a succession of geographical specific points (stops) alongside with their correspondings operated trips.
  */
 export class Route {
-  readonly stops: Array<stopId>;
-  readonly trips: Array<Trip>;
 
   /**
    * @description Creates a new Route. Note that stops and trips are linked : they are cross-connected.
    */
-  constructor(stops: Array<stopId>, trips: Array<Trip>) {
-    this.stops = stops;
-    this.trips = trips;
-  }
+  constructor(readonly id: Id, readonly stops: Array<stopId>, readonly trips: Array<Trip>) { }
 
   /**
    * @description Computes the departure time on a trip at stop p.
@@ -67,9 +44,8 @@ export class Route {
    * @param p Stop index in route (trip).
    */
   departureTime(t: number, p: stopId): timestamp {
-    const trip: Trip = this.trips[t];
-    const pRouteIndex = this.stops[p];
 
-    return trip.stopTimes[pRouteIndex] + trip.stopDurations[pRouteIndex];
+    return this.trips[t].times[this.stops.indexOf(p)][0];
+
   }
 }
