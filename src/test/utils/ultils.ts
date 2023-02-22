311c8bdbb2c97ae1efe9361908cbdf063a06a59b
Catatomik <cata@catadev.org>
2023-02-22 19:59:01 +0100

    ✨ approachedStopName, sectionId

--- src/test/utils/ultils.ts
+++ src/test/utils/ultils.ts
@@ -1,3 +1,5 @@
+import { node } from "../../utils/Graph";
+
 export type resolveCb<T = void> = (value: T) => void;
 export type rejectCb = (reason?: any) => void;
 
@@ -16,4 +18,13 @@ export class Deferred<T = unknown> {
 
 export function euclidianDistance(x1: number, y1: number, x2: number, y2: number): number {
   return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+}
+
+/** Ensure unique node id for approachedStop (as(approached stop)={stop id}) */
+export function approachedStopName(_id: number) {
+  return `as=${_id}` as const;
+}
+
+export function sectionId<S extends { rg_fv_graph_nd: node; rg_fv_graph_na: node }>({ rg_fv_graph_nd, rg_fv_graph_na }: S) {
+  return `${rg_fv_graph_nd}-${rg_fv_graph_na}` as const;
 }
