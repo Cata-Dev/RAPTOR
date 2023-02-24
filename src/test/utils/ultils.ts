50d484523b57871c7620d998fe09afb3a27bb7bc
Catatomik <cata@catadev.org>
2023-02-24 21:19:33 +0100

    ✨ unpackRefType, dbIntersectionId, dbSectionId & moved KeyOfMap

--- src/test/utils/ultils.ts
+++ src/test/utils/ultils.ts
@@ -1,3 +1,5 @@
+import { Ref } from "@typegoose/typegoose";
+import { RefType } from "@typegoose/typegoose/lib/types";
 import proj4, { TemplateCoordinates } from "proj4";
 import { node } from "../../utils/Graph";
 
@@ -21,16 +23,30 @@ export function euclidianDistance(x1: number, y1: number, x2: number, y2: number
   return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
 }
 
+export type unpackRefType<T> = T extends Ref<infer D>
+  ? D extends {
+      _id?: RefType;
+    }
+    ? D["_id"]
+    : never
+  : never;
+
 /** Ensure unique node id for approachedStop (as(approached stop)={stop id}) */
 export function approachedStopName(_id: number) {
   return `as=${_id}` as const;
 }
 
+export function dbIntersectionId(_id: number) {
+  return `i=${_id}` as const;
+}
+
+export function dbSectionId(_id: number) {
+  return `s=${_id}` as const;
+}
+
 export function sectionId<S extends { rg_fv_graph_nd: node; rg_fv_graph_na: node }>({ rg_fv_graph_nd, rg_fv_graph_na }: S) {
   return `${rg_fv_graph_nd}-${rg_fv_graph_na}` as const;
 }
-
-export type KeyOfMap<M extends Map<unknown, unknown>> = M extends Map<infer K, unknown> ? K : never;
 
 /**
  * @description Checks unicity of a value in an array
