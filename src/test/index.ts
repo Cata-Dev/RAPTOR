d125e282fcd926858238bdf1f0a7ea2c1db61d06
Catatomik <cata@catadev.org>
2023-02-25 23:07:37 +0100

    ✨ Use command-line arguments

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,6 +1,13 @@
 import { run } from "./test";
 import { benchmark } from "./utils/benchmark";
 
-benchmark(run, [{ getFullPaths: false, computeGEOJSONs: false }], 5)
+const args = process.argv.slice(2);
+const getFullPaths = JSON.parse(args[0]);
+const times = JSON.parse(args[1]);
+
+if (typeof getFullPaths !== "boolean") throw new Error(`Type of argument 1 "getFullPaths" invalid (got ${typeof getFullPaths})`);
+if (typeof times !== "number") throw new Error(`Type of argument 2 "times" invalid (got ${typeof times})`);
+
+benchmark(run, [{ getFullPaths, computeGEOJSONs: false, dijkstraOptions: { maxCumulWeight: 5_000 } }], times)
   .then(console.log)
   .catch(console.error);
