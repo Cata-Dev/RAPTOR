573c3ecea240e34215d1d8577179c15dda7491cd
Catatomik <cata@catadev.org>
2023-03-02 19:32:49 +0100

    ⚡🐛 Remove unused option

--- src/test/index.ts
+++ src/test/index.ts
@@ -8,6 +8,6 @@ const times = JSON.parse(args[1]);
 if (typeof getFullPaths !== "boolean") throw new Error(`Type of argument 1 "getFullPaths" invalid (got ${typeof getFullPaths})`);
 if (typeof times !== "number") throw new Error(`Type of argument 2 "times" invalid (got ${typeof times})`);
 
-benchmark(run, [{ getFullPaths, computeGEOJSONs: false, dijkstraOptions: { maxCumulWeight: 5_000 } }], times)
+benchmark(run, [{ getFullPaths, dijkstraOptions: { maxCumulWeight: 5_000 } }], this, times)
   .then(console.log)
   .catch(console.error);
