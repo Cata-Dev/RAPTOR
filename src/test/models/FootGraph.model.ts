bd099827af9eecdbd411662465ae1c421e545a93
Catatomik <cata@catadev.org>
2023-03-07 14:50:18 +0100

    🐛 Collection creation of discriminators

--- src/test/models/FootGraph.model.ts
+++ src/test/models/FootGraph.model.ts
@@ -7,7 +7,7 @@ import {
   buildSchema,
   deleteModelWithClass,
   getDiscriminatorModelForClass,
-  getModelForClass,
+  getModelWithString,
   prop,
   Ref,
 } from "@typegoose/typegoose";
@@ -50,9 +50,9 @@ export class dbFootGraphEdges extends dbFootGraph {
 }
 
 export default function init(db: Mongoose) {
-  if (getModelForClass(dbFootGraph, { existingMongoose: db })) deleteModelWithClass(dbFootGraph);
-  if (getModelForClass(dbFootGraphNodes, { existingMongoose: db })) deleteModelWithClass(dbFootGraphNodes);
-  if (getModelForClass(dbFootGraphEdges, { existingMongoose: db })) deleteModelWithClass(dbFootGraphEdges);
+  if (getModelWithString(getName(dbFootGraph))) deleteModelWithClass(dbFootGraph);
+  if (getModelWithString(getName(dbFootGraphNodes))) deleteModelWithClass(dbFootGraphNodes);
+  if (getModelWithString(getName(dbFootGraphEdges))) deleteModelWithClass(dbFootGraphEdges);
 
   const dbFootGraphSchema = buildSchema(dbFootGraph, { existingMongoose: db });
   const dbFootGraphModelRaw = db.model(getName(dbFootGraph), dbFootGraphSchema);
