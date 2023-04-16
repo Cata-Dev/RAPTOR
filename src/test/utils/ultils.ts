6d4e273ae61b7138149f0c96ca8684f5c33385a0
Catatomik <cata@catadev.org>
2023-04-16 02:26:29 +0200

    ✨ binarySearch & binaryFilter, complete unpackRefType

--- src/test/utils/ultils.ts
+++ src/test/utils/ultils.ts
@@ -28,6 +28,12 @@ export type unpackRefType<T> = T extends Ref<infer D>
     }
     ? D["_id"]
     : never
+  : T extends Ref<infer D>[]
+  ? D extends {
+      _id?: RefType;
+    }
+    ? D["_id"][]
+    : never
   : never;
 
 /** Ensure unique node id for approachedStop (as(approached stop)={stop id}) */
@@ -52,4 +58,46 @@ export function sectionId<S extends { rg_fv_graph_nd: node; rg_fv_graph_na: node
  */
 export function unique<T>(v: T, i: number, arr: T[]): boolean {
   return arr.indexOf(v) === i;
+}
+
+/**
+ * @description Search for a value in a **sorted** array, in O(log2(n)).
+ * @param arr The **sorted** array where performing the search
+ * @param el The element to look for, which will be compared
+ * @param compare A compare function that takes 2 arguments : `a` el and `b` an element of the array.
+ * It returns :
+ *    - a negative number if `a` is before `b`;
+ *    - 0 if `a` is equal to `b`;
+ *    - a positive number of `a` is after `b`.
+ * @returns The index of el if positive ; index of insertion if negative
+ */
+export function binarySearch<T, C>(arr: T[], el: C, compare: (a: C, b: T) => number) {
+  let low = 0;
+  let high = arr.length - 1;
+  while (low <= high) {
+    const mid = (high + low) >> 1; // x >> 1 == Math.floor(x/2)
+    const cmp = compare(el, arr[mid]);
+    if (cmp > 0) {
+      low = mid + 1;
+    } else if (cmp < 0) {
+      high = mid - 1;
+    } else {
+      return mid;
+    }
+  }
+  return ~low; // ~x == -x-1
+}
+
+export function binaryFilter<T, C>(arr: T[], el: C, compare: (a: C, b: T) => number): T[] {
+  const binarySearchResult = binarySearch(arr, el, compare);
+  if (binarySearchResult < 0) return [];
+  let low = binarySearchResult;
+  while (compare(el, arr[low]) === 0) {
+    low--;
+  }
+  let high = binarySearchResult;
+  while (compare(el, arr[high]) === 0) {
+    high--;
+  }
+  return arr.slice(low, high + 1);
 }
