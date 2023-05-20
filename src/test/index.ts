8605f6fe4b2c0cb2d67631915cc507008724200b
Catatomik <cata@catadev.org>
2023-05-21 01:23:41 +0200

    🚧 Use getBestJourneys

--- src/test/index.ts
+++ src/test/index.ts
@@ -125,7 +125,7 @@ import { benchmark } from "./utils/benchmark";
   if (!b3.lastReturn) throw `b3 return null`;
 
   function resultRAPTOR() {
-    return RAPTORInstance.getBestJourney(ps, pt);
+    return RAPTORInstance.getBestJourneys(ps, pt);
   }
   const b4 = await benchmark(resultRAPTOR, []);
   console.log("b4 ended");
