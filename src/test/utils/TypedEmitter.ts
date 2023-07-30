8e5824bafa5293fa7ec08f0208b85d232ddc1530
Catatomik <cata@catadev.org>
2023-07-31 00:21:34 +0200

    🐛 no-unsafe-declaration-merging
    `number` cannot be an EventName?!

--- src/test/utils/TypedEmitter.ts
+++ src/test/utils/TypedEmitter.ts
@@ -3,11 +3,11 @@ import { EventEmitter } from "events";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type EmittedEvents = Record<string | symbol, (...args: any) => any>;
 
-export declare interface TypedEventEmitter<Events extends EmittedEvents> {
-  on<E extends keyof Events>(event: E, listener: Events[E]): this;
+export declare interface ITypedEventEmitter<Events extends EmittedEvents> {
+  on<E extends keyof Events>(event: Exclude<E, number>, listener: Events[E]): this;
 
-  emit<E extends keyof Events>(event: E, ...args: Parameters<Events[E]>): boolean;
+  emit<E extends keyof Events>(event: Exclude<E, number>, ...args: Parameters<Events[E]>): boolean;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export class TypedEventEmitter<Events extends EmittedEvents> extends EventEmitter {}
+export class TypedEventEmitter<Events extends EmittedEvents> extends EventEmitter implements ITypedEventEmitter<Events> {}
