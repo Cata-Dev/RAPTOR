e0327a529502660f868b4eca98946e13b1074458
Catatomik <cata@catadev.org>
2024-02-05 11:19:32 +0100

    🔧

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,16 @@
 /* eslint-env node */
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended-type-checked',
-    'plugin:@typescript-eslint/stylistic-type-checked',
-  ],
-  parser: '@typescript-eslint/parser',
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended-type-checked", "plugin:@typescript-eslint/stylistic-type-checked"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
     tsconfigRootDir: __dirname,
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ["@typescript-eslint"],
   root: true,
   rules: {
-    "no-empty": [ "error", { allowEmptyCatch: true } ],
-    "@typescript-eslint/restrict-template-expressions": "warn"
-  }
+    "no-empty": ["error", { allowEmptyCatch: true }],
+    "@typescript-eslint/restrict-template-expressions": "warn",
+    "@typescript-eslint/no-unused-vars": ["error", { caughtErrorsIgnorePattern: "^_+$", argsIgnorePattern: "^_+$", varsIgnorePattern: "^_+$" }],
+  },
 };
