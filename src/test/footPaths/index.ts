8867ff13d7b067a36a968272ef303e1a510d6e30
Catatomik <cata@catadev.org>
2024-10-04 23:14:51 +0200

    ✨ Enhance test index arguments

--- src/test/footPaths/index.ts
+++ src/test/footPaths/index.ts
@@ -3,11 +3,13 @@ import { benchmark } from "../utils/benchmark";
 
 const args = process.argv.slice(2);
 const getFullPaths = JSON.parse(args[0]) as boolean;
-const times = JSON.parse(args[1]) as number;
+const computeGEOJSONs = JSON.parse(args[1]) as boolean;
+const times = JSON.parse(args[2]) as number;
 
 if (typeof getFullPaths !== "boolean") throw new Error(`Type of argument 1 "getFullPaths" invalid (got ${typeof getFullPaths})`);
-if (typeof times !== "number") throw new Error(`Type of argument 2 "times" invalid (got ${typeof times})`);
+if (typeof computeGEOJSONs !== "boolean") throw new Error(`Type of argument 2 "computeGEOJSONs" invalid (got ${typeof getFullPaths})`);
+if (typeof times !== "number") throw new Error(`Type of argument 3 "times" invalid (got ${typeof times})`);
 
-benchmark(run, [{ getFullPaths, dijkstraOptions: { maxCumulWeight: 5_000 } }], this, times)
-  .then(console.log)
+benchmark(run, [{ getFullPaths, computeGEOJSONs, dijkstraOptions: { maxCumulWeight: 5_000 } }], this, times)
+  .then()
   .catch(console.error);
