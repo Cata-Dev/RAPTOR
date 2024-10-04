f26e97cac9e2f48fc769d6ad665174b03161853e
Catatomik <cata@catadev.org>
2024-10-04 23:40:31 +0200

    🐛 Fix departureTime

--- src/test/index.ts
+++ src/test/index.ts
@@ -19,7 +19,7 @@ import { inspect } from "util";
 async function init() {
   const db = await initDB();
   const stopsModel = stopsModelInit(db);
-  TBMSchedulesModelInit(db);
+  const TBMSchedulesModel = TBMSchedulesModelInit(db)[1];
   const TBMScheduledRoutesModel = TBMScheduledRoutesModelInit(db);
   const NonScheduledRoutesModel = NonScheduledRoutesModelInit(db);
 
@@ -117,10 +117,10 @@ async function init() {
   if (!b2.lastReturn) throw new Error(`b2 return null`);
   const { RAPTORInstance } = b2.lastReturn;
 
-  return { RAPTORInstance, TBMScheduledRoutesModel };
+  return { RAPTORInstance, TBMSchedulesModel };
 }
 
-async function run({ RAPTORInstance, TBMScheduledRoutesModel }: Awaited<ReturnType<typeof init>>) {
+async function run({ RAPTORInstance, TBMSchedulesModel }: Awaited<ReturnType<typeof init>>) {
   const args = process.argv.slice(2);
   let ps: number;
   try {
@@ -132,10 +132,10 @@ async function run({ RAPTORInstance, TBMScheduledRoutesModel }: Awaited<ReturnTy
   try {
     pt = JSON.parse(args[1]) as number;
   } catch (_) {
-    pt = 2168;
+    pt = 2082;
   }
 
-  const minSchedule = (await TBMScheduledRoutesModel.findOne({}).lean())?.updatedAt?.getTime() ?? Infinity;
+  const minSchedule = (await TBMSchedulesModel.findOne({}).lean())?.updatedAt?.getTime() ?? Infinity;
 
   function runRAPTOR() {
     RAPTORInstance.run(ps, pt, minSchedule, { walkSpeed: 1.5 });
