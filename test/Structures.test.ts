028d727fe067a52c5e06a91f479f1ddcddec7fea
Catatomik <cata@catadev.org>
2025-06-24 14:15:55 +0200

    ✅

--- test/Structures.test.ts
+++ test/Structures.test.ts
@@ -40,5 +40,7 @@ describe("Route class", () => {
     expect(route.departureTime(0, 2)).toBe(5);
     expect(route.departureTime(1, 0)).toBe(4);
     expect(route.departureTime(1, 2)).toBe(12);
+    expect(() => route.departureTime(2, 0)).toThrow("No departure time");
+    expect(() => route.departureTime(0, 3)).toThrow("No departure time");
   });
 });
