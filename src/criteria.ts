b2479e64972308a7239da2d33f6fca50d04a80af
Catatomik <cata@catadev.org>
2025-06-25 18:06:53 +0200

    🐛 bufferTime initial value

--- src/criteria.ts
+++ src/criteria.ts
@@ -14,7 +14,7 @@ function isCriterionJourneyStepFoot<SI extends Id, RI extends Id, C extends stri
 
 const bufferTime: Criterion<Id, Id, ["bufferTime"]> = {
   name: "bufferTime",
-  initialValue: -Infinity,
+  initialValue: 0,
   update: (prefixJourney, newJourneyStep) => {
     const lastJourneyStep = prefixJourney.at(-1);
     if (!lastJourneyStep) throw new Error("A journey should at least contain the DEPARTURE label.");
