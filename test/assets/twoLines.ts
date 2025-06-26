d5a84ac4411a70bc4540487724d9d731629f1c60
Catatomik <cata@catadev.org>
2025-06-26 14:11:27 +0200

    🎨 Better factorization

--- test/assets/twoLines.ts
+++ test/assets/twoLines.ts
@@ -1,78 +1,78 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { describe, expect, test } from "@jest/globals";
-import { FootPath } from "../../src/main";
+import { FootPath, Journey } from "../../src/main";
 import { TestAsset, TestDataset } from "./asset";
 
 const MAX_ROUNDS = 6;
 const PARAMS: TestAsset["tests"][number]["params"] = [1, 7, 0, { walkSpeed: 1 }, MAX_ROUNDS];
 
-const baseValidate234: TestAsset["tests"][number]["validate"] = (res) => {
-  test("Run result is exact (generic k=2,3,4...)", () => {
-    for (let i = 4; i < MAX_ROUNDS; ++i) expect(res[i]).toBe(null);
-    for (const i of [2, 3]) {
-      expect(res[i]!.length).toBe(3);
-
-      const js0 = res[i]![0];
-      expect(Object.keys(js0).length).toBe(2);
-      expect(Object.keys(js0)).toContain("compare");
-      expect(Object.keys(js0)).toContain("label");
-      expect(js0.label.time).toBe(0);
-
-      expect(Object.keys(res[i]![1]).length).toEqual(5);
-      expect(res[i]![1].label.time).toBe(4);
-
-      const js1 = res[i]![1];
-      if (!("route" in js1)) throw new Error("First journey step isn't VEHICLE");
-
-      expect(js1.boardedAt).toBe(1);
-      expect(js1.route.id).toBe(1);
-      expect(js1.tripIndex).toBe(0);
-
-      expect(Object.keys(res[i]![2]).length).toEqual(5);
-      expect(res[i]![2].label.time).toBe(10);
-
-      const js2 = res[i]![2];
-      if (!("route" in js2)) throw new Error("Second journey step isn't VEHICLE");
-
-      expect(js2.boardedAt).toBe(3);
-      expect(js2.route.id).toBe(2);
-      expect(js2.tripIndex).toBe(1);
-    }
+const baseValidateN = (journey: Journey<number, number, []> | null) => {
+  test("Run result is exact (generic null)", () => {
+    expect(journey).toBe(null);
   });
 };
 
-const baseValidate1: TestAsset["tests"][number]["validate"] = (res) => {
-  test("Run result is exact (generic k=0,1)", () => {
-    for (let i = 0; i < 1; ++i) expect(res[i]).toBe(null);
-    for (const i of [1]) {
-      expect(res[i]!.length).toBe(3);
+const baseValidateVV = (journey: Journey<number, number, []>) => {
+  test("Run result is exact (generic VEHICLE+VEHICLE)", () => {
+    expect(journey.length).toBe(3);
 
-      const js0 = res[i]![0];
-      expect(Object.keys(js0).length).toBe(2);
-      expect(Object.keys(js0)).toContain("compare");
-      expect(Object.keys(js0)).toContain("label");
-      expect(js0.label.time).toBe(0);
+    const js0 = journey[0];
+    expect(Object.keys(js0).length).toBe(2);
+    expect(Object.keys(js0)).toContain("compare");
+    expect(Object.keys(js0)).toContain("label");
+    expect(js0.label.time).toBe(0);
 
-      expect(Object.keys(res[i]![1]).length).toEqual(5);
-      expect(res[i]![1].label.time).toBe(6);
+    expect(Object.keys(journey[1]).length).toEqual(5);
+    expect(journey[1].label.time).toBe(4);
 
-      const js1 = res[i]![1];
-      if (!("route" in js1)) throw new Error("First journey step isn't VEHICLE");
+    const js1 = journey[1];
+    if (!("route" in js1)) throw new Error("First journey step isn't VEHICLE");
 
-      expect(js1.boardedAt).toBe(1);
-      expect(js1.route.id).toBe(1);
-      expect(js1.tripIndex).toBe(0);
+    expect(js1.boardedAt).toBe(1);
+    expect(js1.route.id).toBe(1);
+    expect(js1.tripIndex).toBe(0);
 
-      expect(Object.keys(res[i]![2]).length).toEqual(4);
-      expect(res[i]![2].label.time).toBe(11);
+    expect(Object.keys(journey[2]).length).toEqual(5);
+    expect(journey[2].label.time).toBe(10);
 
-      const js2 = res[i]![2];
-      if (!("transfer" in js2)) throw new Error("Second journey step isn't FOOT");
+    const js2 = journey[2];
+    if (!("route" in js2)) throw new Error("Second journey step isn't VEHICLE");
 
-      expect(js2.boardedAt).toBe(4);
-      expect(js2.transfer.to).toBe(7);
-      expect(js2.transfer.length).toBe(5);
-    }
+    expect(js2.boardedAt).toBe(3);
+    expect(js2.route.id).toBe(2);
+    expect(js2.tripIndex).toBe(1);
+  });
+};
+
+const baseValidateVF = (journey: Journey<number, number, []>) => {
+  test("Run result is exact (generic VEHICLE+FOOT)", () => {
+    expect(journey.length).toBe(3);
+
+    const js0 = journey[0];
+    expect(Object.keys(js0).length).toBe(2);
+    expect(Object.keys(js0)).toContain("compare");
+    expect(Object.keys(js0)).toContain("label");
+    expect(js0.label.time).toBe(0);
+
+    expect(Object.keys(journey[1]).length).toEqual(5);
+    expect(journey[1].label.time).toBe(6);
+
+    const js1 = journey[1];
+    if (!("route" in js1)) throw new Error("First journey step isn't VEHICLE");
+
+    expect(js1.boardedAt).toBe(1);
+    expect(js1.route.id).toBe(1);
+    expect(js1.tripIndex).toBe(0);
+
+    expect(Object.keys(journey[2]).length).toEqual(4);
+    expect(journey[2].label.time).toBe(11);
+
+    const js2 = journey[2];
+    if (!("transfer" in js2)) throw new Error("Second journey step isn't FOOT");
+
+    expect(js2.boardedAt).toBe(4);
+    expect(js2.transfer.to).toBe(7);
+    expect(js2.transfer.length).toBe(5);
   });
 };
 
@@ -146,10 +146,9 @@ export default {
       {
         params: PARAMS,
         validate: (res) => {
-          test("Run result is exact", () => {
-            for (let i = 0; i < 2; ++i) expect(res[i]).toBe(null);
-          });
-          baseValidate234(res);
+          for (let i = 0; i < 2; ++i) baseValidateN(res[i]);
+          for (const i of [2, 3]) baseValidateVV(res[i]!);
+          for (let i = 4; i < MAX_ROUNDS; ++i) baseValidateN(res[i]);
         },
       },
     ],
@@ -227,8 +226,10 @@ export default {
       {
         params: PARAMS,
         validate: (res) => {
-          baseValidate1(res);
-          baseValidate234(res);
+          for (let i = 0; i < 1; ++i) baseValidateN(res[i]);
+          baseValidateVF(res[1]!);
+          for (const i of [2, 3]) baseValidateVV(res[i]!);
+          for (let i = 4; i < MAX_ROUNDS; ++i) baseValidateN(res[i]);
         },
       },
     ],
@@ -314,7 +315,7 @@ export default {
         params: PARAMS,
         validate: (res) => {
           describe("Run result is exact, early departure", () => {
-            baseValidate1(res);
+            baseValidateVF(res[1]!);
             test("k=2,3,4...", () => {
               for (const i of [2, 3]) {
                 expect(res[i]!.length).toBe(4);
@@ -429,3 +430,5 @@ export default {
     ],
   },
 } satisfies TestDataset;
+
+export { MAX_ROUNDS };
