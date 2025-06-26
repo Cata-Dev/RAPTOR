984b44df5eb7b6932e70aef76fa77c4fbc9adf3a
Catatomik <cata@catadev.org>
2025-06-26 17:30:45 +0200

    🔥

--- src/shared.ts
+++ src/shared.ts
@@ -1,10 +1,5 @@
-import BaseRAPTOR from "./base";
 import RAPTOR from "./RAPTOR";
 import { SerializedId, SharedRAPTORData } from "./structures";
-
-export class BaseSharedRAPTOR extends BaseRAPTOR<number | SerializedId, number | SerializedId, number> {
-  static readonly dataClass = SharedRAPTORData;
-}
 
 export class SharedRAPTOR extends RAPTOR<number | SerializedId, number | SerializedId, number> {
   static readonly dataClass = SharedRAPTORData;
