f0984b1eaad9d041c080e7046c6fa880e7e70b57
Catatomik <cata@catadev.org>
2025-07-03 21:50:53 +0200

    🔧 Test in source

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 /** @type {import("jest").Config} **/
 module.exports = {
   collectCoverageFrom: ["src/**/*.ts"],
+  coveragePathIgnorePatterns: ["src/test/"],
   coverageReporters: ["text", "lcov", "json-summary"],
   testMatch: ["**/test/**/*.test.ts"],
   testEnvironment: "node",
