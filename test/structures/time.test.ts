0d37e252de078e621d7b182d650542de2cfceb9b
Catatomik <cata@catadev.org>
2025-07-10 10:24:13 +0200

    ✅ Time

--- test/structures/time.test.ts
+++ test/structures/time.test.ts
@@ -1,34 +1,56 @@
 import { describe, expect, test } from "@jest/globals";
-import { MAX_SAFE_TIMESTAMP, TimeScal } from "../../src";
+import { makeTime, MAX_SAFE_TIMESTAMP, Time, TimeScal } from "../../src";
+
+function testTimeScalProps(timeScal: Time<number>) {
+  expect(timeScal.MAX).toBe(Infinity);
+  expect(timeScal.MAX_SAFE).toBe(MAX_SAFE_TIMESTAMP);
+  expect(timeScal.MIN).toBe(-Infinity);
+}
+
+function testTimeScalMethods(timeScal: Time<number>) {
+  expect(timeScal.max()).toBe(-Infinity);
+  expect(timeScal.max(-5, 0, -3.1, 2, 1.2)).toBe(2);
+
+  expect(timeScal.min()).toBe(Infinity);
+  expect(timeScal.min(-5, 0, -3.1, 2, 1.2)).toBe(-5);
+
+  expect(timeScal.order(0, 0) === 0).toBe(true);
+  expect(timeScal.order(0, 3) < 0).toBe(true);
+  expect(timeScal.order(3, 0) > 0).toBe(true);
+  expect(timeScal.order(0, 3.3) < 0).toBe(true);
+  expect(timeScal.order(3.3, 0) > 0).toBe(true);
+  expect(timeScal.order(-Infinity, Infinity) < 0).toBe(true);
+
+  expect(timeScal.plusScal(0, 0)).toBe(0);
+  expect(timeScal.plusScal(1, 3)).toBe(4);
+  expect(timeScal.plusScal(3, 1)).toBe(4);
+  expect(timeScal.plusScal(1, 3.3)).toBe(4.3);
+  expect(timeScal.plusScal(1, Infinity)).toBe(Infinity);
+  expect(timeScal.plusScal(Infinity, 1)).toBe(Infinity);
+  expect(timeScal.plusScal(1, -Infinity)).toBe(-Infinity);
+  expect(timeScal.plusScal(-Infinity, 1)).toBe(-Infinity);
+}
 
 describe("timeScal", () => {
   test("Props are correct", () => {
-    expect(TimeScal.MAX).toBe(Infinity);
-    expect(TimeScal.MAX_SAFE).toBe(MAX_SAFE_TIMESTAMP);
-    expect(TimeScal.MIN).toBe(-Infinity);
+    testTimeScalProps(TimeScal);
   });
 
   test("Methods behave as expected", () => {
-    expect(TimeScal.max()).toBe(-Infinity);
-    expect(TimeScal.max(-5, -3.1, 0, 1.2, 2)).toBe(2);
+    testTimeScalMethods(TimeScal);
+  });
+});
 
-    expect(TimeScal.min()).toBe(Infinity);
-    expect(TimeScal.min(-5, -3.1, 0, 1.2, 2)).toBe(-5);
+describe("makeTime", () => {
+  describe("with TimeScal", () => {
+    const timeScalMade = makeTime(TimeScal.order, TimeScal.MAX_SAFE, TimeScal.MAX, TimeScal.MIN, TimeScal.plusScal);
 
-    expect(TimeScal.order(0, 0) === 0).toBe(true);
-    expect(TimeScal.order(0, 3) < 0).toBe(true);
-    expect(TimeScal.order(3, 0) > 0).toBe(true);
-    expect(TimeScal.order(0, 3.3) < 0).toBe(true);
-    expect(TimeScal.order(3.3, 0) > 0).toBe(true);
-    expect(TimeScal.order(-Infinity, Infinity) < 0).toBe(true);
+    test("Props are correct", () => {
+      testTimeScalProps(timeScalMade);
+    });
 
-    expect(TimeScal.plusScal(0, 0)).toBe(0);
-    expect(TimeScal.plusScal(1, 3)).toBe(4);
-    expect(TimeScal.plusScal(3, 1)).toBe(4);
-    expect(TimeScal.plusScal(1, 3.3)).toBe(4.3);
-    expect(TimeScal.plusScal(1, Infinity)).toBe(Infinity);
-    expect(TimeScal.plusScal(Infinity, 1)).toBe(Infinity);
-    expect(TimeScal.plusScal(1, -Infinity)).toBe(-Infinity);
-    expect(TimeScal.plusScal(-Infinity, 1)).toBe(-Infinity);
+    test("Methods behave as expected", () => {
+      testTimeScalMethods(timeScalMade);
+    });
   });
 });
