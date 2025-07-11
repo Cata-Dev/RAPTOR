5143db46682babd77b47ce50871c0de7d951070a
Catatomik <cata@catadev.org>
2025-07-11 10:03:54 +0200

    🎨

--- src/test/models/index.ts
+++ src/test/models/index.ts
@@ -1,12 +1,13 @@
-import { dbAddresses, dbAddressesModel } from "./addresses.model";
-import { dbIntersections, dbIntersectionsModel } from "./intersections.model";
-import { dbSections, dbSectionsModel } from "./sections.model";
-import { dbTBM_Lines, dbTBM_LinesModel } from "./TBM_lines.model";
-import { dbTBM_Lines_routes, dbTBM_Lines_routesModel } from "./TBM_lines_routes.model";
-import { dbTBM_Schedules, dbTBM_Schedules_rt, dbTBM_Schedules_rtModel, dbTBM_SchedulesModel } from "./TBM_schedules.model";
-import { dbTBM_Stops, dbTBM_StopsModel } from "./TBM_stops.model";
-import { dbTBM_Trips, dbTBM_TripsModel } from "./TBM_trips.model";
-import { dbTBM_ScheduledRoutes, dbTBM_ScheduledRoutesModel } from "./TBMScheduledRoutes.model";
+import { ReturnModelType } from "@typegoose/typegoose";
+import { dbAddresses } from "./addresses.model";
+import { dbIntersections } from "./intersections.model";
+import { dbSections } from "./sections.model";
+import { dbTBM_Lines } from "./TBM_lines.model";
+import { dbTBM_Lines_routes } from "./TBM_lines_routes.model";
+import { dbTBM_Schedules, dbTBM_Schedules_rt } from "./TBM_schedules.model";
+import { dbTBM_Stops } from "./TBM_stops.model";
+import { dbTBM_Trips } from "./TBM_trips.model";
+import { dbTBM_ScheduledRoutes } from "./TBMScheduledRoutes.model";
 
 enum TBMEndpoints {
   Addresses = "Addresses",
@@ -25,48 +26,28 @@ enum TBMEndpoints {
 }
 
 type TBMClass<E extends TBMEndpoints = TBMEndpoints> = E extends TBMEndpoints.Addresses
-  ? dbAddresses
+  ? typeof dbAddresses
   : E extends TBMEndpoints.Intersections
-    ? dbIntersections
+    ? typeof dbIntersections
     : E extends TBMEndpoints.Sections
-      ? dbSections
+      ? typeof dbSections
       : E extends TBMEndpoints.Lines
-        ? dbTBM_Lines
+        ? typeof dbTBM_Lines
         : E extends TBMEndpoints.Lines_routes
-          ? dbTBM_Lines_routes
+          ? typeof dbTBM_Lines_routes
           : E extends TBMEndpoints.Schedules
-            ? dbTBM_Schedules
+            ? typeof dbTBM_Schedules
             : E extends TBMEndpoints.Schedules_rt
-              ? dbTBM_Schedules_rt
+              ? typeof dbTBM_Schedules_rt
               : E extends TBMEndpoints.Stops
-                ? dbTBM_Stops
+                ? typeof dbTBM_Stops
                 : E extends TBMEndpoints.Trips
-                  ? dbTBM_Trips
+                  ? typeof dbTBM_Trips
                   : E extends TBMEndpoints.ScheduledRoutes
-                    ? dbTBM_ScheduledRoutes
+                    ? typeof dbTBM_ScheduledRoutes
                     : never;
 
-type TBMModel<E extends TBMEndpoints = TBMEndpoints> = E extends TBMEndpoints.Addresses
-  ? dbAddressesModel
-  : E extends TBMEndpoints.Intersections
-    ? dbIntersectionsModel
-    : E extends TBMEndpoints.Sections
-      ? dbSectionsModel
-      : E extends TBMEndpoints.Lines
-        ? dbTBM_LinesModel
-        : E extends TBMEndpoints.Lines_routes
-          ? dbTBM_Lines_routesModel
-          : E extends TBMEndpoints.Schedules
-            ? dbTBM_SchedulesModel
-            : E extends TBMEndpoints.Schedules_rt
-              ? dbTBM_Schedules_rtModel
-              : E extends TBMEndpoints.Stops
-                ? dbTBM_StopsModel
-                : E extends TBMEndpoints.Trips
-                  ? dbTBM_TripsModel
-                  : E extends TBMEndpoints.ScheduledRoutes
-                    ? dbTBM_ScheduledRoutesModel
-                    : never;
+type TBMModel<E extends TBMEndpoints = TBMEndpoints> = ReturnModelType<TBMClass<E>>;
 
 export { TBMEndpoints };
 export type { TBMClass, TBMModel };
