804b8116adb5b9da5522773cfc837f882c8eebe6
Catatomik <cata@catadev.org>
2025-07-22 17:41:38 +0200

    ✨ Bench format + data type + spi criterion

--- bench.sh
+++ bench.sh
@@ -12,10 +12,16 @@ while getopts 'gct:d:' ARG; do
     ;;
   t)
     TIMES="$OPTARG"
-    echo "$TIMES" | grep -E '^[0-9]+$' || { echo "Invalid times to run: $TIMES"; exit 1; }
+    echo "$TIMES" | grep -E '^[0-9]+$' || {
+      echo "Invalid times to run: $TIMES"
+      exit 1
+    }
     ;;
   d)
-    echo "$OPTARG" | grep -E '^[0-9]+$' || { echo "Invalid run transfer max len: $OPTARG"; exit 1; }
+    echo "$OPTARG" | grep -E '^[0-9]+$' || {
+      echo "Invalid run transfer max len: $OPTARG"
+      exit 1
+    }
     FOOT_DISTANCE="--fp-run-len $OPTARG"
     ;;
   *)
@@ -33,50 +39,53 @@ pnpm run build
 mkdir bench 2>/dev/null || true
 
 if [ -n "$GLOBAL" ]; then
-  for INSTANCE in \
-    'r' \
-    'sr' \
-    'mcr' \
-    'mcsr'
-  do
-    FOLDER="bench/$INSTANCE"
-    mkdir "$FOLDER" 2>/dev/null || true
-    echo "Bench instance $INSTANCE"
-    pnpm exec 0x -D "$FOLDER" lib/test/index.js -i "$INSTANCE" "$FOOT_DISTANCE" --runTimes "$TIMES" 1>"$FOLDER/out.txt" 2>/dev/null
-    sed -i "s|$(dirname "$PWD")/||g" "$FOLDER/flamegraph.html"
-    echo "Done at: $FOLDER/flamegraph.html"
+  for DTYPE in \
+    'scal' \
+    'int'; do
+    for INSTANCE in \
+      'r' \
+      'sr' \
+      'mcr' \
+      'mcsr'; do
+      FOLDER="bench/$DTYPE-$INSTANCE"
+      mkdir "$FOLDER" 2>/dev/null || true
+      echo "Bench instance $INSTANCE"
+      pnpm exec 0x -D "$FOLDER" lib/test/index.js -d "$DTYPE" -i "$INSTANCE" "$FOOT_DISTANCE" --runTimes "$TIMES" 1>"$FOLDER/out.txt" 2>"$FOLDER/err.txt"
+      sed -i "s|$(dirname "$PWD")/||g" "$FOLDER/flamegraph.html"
+      echo "Done at: $FOLDER/flamegraph.html"
+    done
   done
 fi
 
 if [ -n "$CRITERIA" ]; then
   # Multi-criteria specific
-  for INSTANCE in \
-    'mcr' \
-    'mcsr'
-  do
-    for CRITERIA in \
-      '--fd' \
-      '--bt' \
-      # Might be very long...
-      # '--fd --bt'
-    do
-      for DELAY in \
-        '--delay-pos=1 --delay-neg=0' \
-        '--delay-pos=2 --delay-neg=1' \
-        '--delay-pos=3 --delay-neg=2' \
-        # Might be very long...
-        # '--fd --bt'
-      do
-        FOLDER="bench/$INSTANCE$(echo "$CRITERIA" | sed 's/ //g')$(echo "$DELAY" | sed -E 's/( ?--delay|=)//g')"
-        mkdir "$FOLDER" 2>/dev/null || true
-        echo "Bench instance $INSTANCE with criteria $CRITERIA and delays $DELAY"
-        # shellcheck disable=SC2086
-        pnpm exec 0x -D "$FOLDER" lib/test/index.js -i "$INSTANCE" "$FOOT_DISTANCE" $CRITERIA $DELAY --runTimes "$TIMES" 1>"$FOLDER/out.txt" 2>/dev/null
-        sed -i "s|$(dirname "$PWD")/||g" "$FOLDER/flamegraph.html"
-        echo "Done at: $FOLDER/flamegraph.html"
+  for DTYPE in \
+    'scal' \
+    'int'; do
+    for INSTANCE in \
+      'mcr' \
+      'mcsr'; do
+      for CRITERIA in \
+        '--fd' \
+        '--bt' \
+        '--spi' \
+        '--bt --spi'; do
+        # '--fd --bt' # Might be very long...
+        for DELAY in \
+          '--delay-pos=1 --delay-neg=0' \
+          '--delay-pos=2 --delay-neg=1' \
+          '--delay-pos=3 --delay-neg=2'; do
+          FOLDER="bench/$DTYPE-$INSTANCE$(echo "$CRITERIA" | sed 's/ //g')$(echo "$DELAY" | sed -E 's/( ?--delay|=)//g')"
+          mkdir "$FOLDER" 2>/dev/null || true
+          echo "Bench instance $INSTANCE with criteria $CRITERIA and delays $DELAY"
+          # shellcheck disable=SC2086
+          pnpm exec 0x -D "$FOLDER" lib/test/index.js -d "$DTYPE" -i "$INSTANCE" "$FOOT_DISTANCE" $CRITERIA $DELAY --runTimes "$TIMES" 1>"$FOLDER/out.txt" 2>"$FOLDER/err.txt"
+          sed -i "s|$(dirname "$PWD")/||g" "$FOLDER/flamegraph.html"
+          echo "Done at: $FOLDER/flamegraph.html"
+        done
       done
     done
   done
 fi
 
-# UI doc https://github.com/davidmarkclements/0x/blob/master/docs/ui.md
+# flamegraph UI doc https://github.com/davidmarkclements/0x/blob/master/docs/ui.md
